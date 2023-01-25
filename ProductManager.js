ab773da1a63de8c962d241264caf4a134ac71041
danieljlugo <daniellugofreelancer@gmail.com>
2023-01-25 16:39:56 -0300

    Desafio 2 Backend

--- ProductManager.js
+++ ProductManager.js
@@ -19,94 +19,156 @@ class ProductManager {
     this.patch = "./products.txt"
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    
+    
+
     try {
-      for (let i = 0; i < this.products.length; i++) {
-        if (this.products[i].code == code) {
-          return console.log("Product Code already exist");
-        } else {
-          console.log("Product added");
-        }
+      if (this.findProductByCode(code)) {
+        return console.log("Product Code already exist");
       }
 
-      let item = new Product(title, description, price, thumbnail, code, stock);
+      const item = new Product(title, description, price, thumbnail, code, stock);
 
-      if (Object.values(item).includes(undefined)) {
-        console.log("All fields are required");
+      if (this.validateProduct(item)) {
+        const product = { ...item, id: this.id++ };
+        this.products.push(product);
+        await fs.writeFile(this.patch, JSON.stringify(this.products));
+        console.log("Product added");
       } else {
-        this.products.push({ ...item, id: this.id++ });
+        console.log("All fields are required");
       }
     } catch (error) {
       console.log(error);
     }
   }
 
-  getProducts() {
-    return this.products;
+  readAllProducts = async ()=>{
+    let response =  await fs.readFile(this.patch, "utf-8")
+    return  JSON.parse(response)
   }
 
-  getProductsById(id) {
-    let product = this.products.find((item) => item.id === id);
+  getProducts = async () => {
+    let response = await this.readAllProducts()
+    console.log(response)
+    return response
+  }
+
+  getProductsById = async (id) => {
+    let response =  await this.readAllProducts()
+
+    let product = response.find((item) => item.id === id);
 
     if (!product) {
-      console.log("Not Found");
+      console.log("Product Not Found");
       return null;
     }
-
+    console.log(product)
     return product;
+  }
+
+  deleteProductoById = async (id)=>{
+
+    let response = await this.readAllProducts();
+    let responseFilter = response.filter(products => products.id != id)
+
+    await fs.writeFile(this.patch, JSON.stringify(responseFilter))
+    console.log(`${id} has been deleted`)
+    console.log(responseFilter)
+    
+
+
+  }
+
+
+  async updateProduct(id, updatedProduct) {
+    try {
+      const products = await this.readAllProducts();
+      const productIndex = products.findIndex((item) => item.id === id);
+
+      if (productIndex === -1) {
+        console.log("Product Not Found");
+        return;
+      }
+
+      const product = { ...products[productIndex], ...updatedProduct };
+      products[productIndex] = product;
+      await fs.writeFile(this.patch, JSON.stringify(products));
+      console.log("Product updated");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
+
+  findProductByCode(code) {
+    return this.products.find((item) => item.code === code);
+  }
+
+  validateProduct(product) {
+    return Object.values(product).every((value) => value !== undefined);
   }
 }
 
 const newCollection = new ProductManager();
 
-newCollection.addProduct(
-  "Poroto",
-  "Porotos Negros",
-  1000,
-  "url:pic",
-  "001",
-  100
-);
+// newCollection.addProduct(
+//   "Poroto",
+//   "Porotos Negros",
+//   1000,
+//   "url:pic",
+//   "001",
+//   100
+// );
 
-newCollection.addProduct(
-  "Poroto",
-  "Porotos Rojos",
-  2500,
-  "url:pic",
-  "002",
-  200
-);
+// newCollection.addProduct(
+//   "Poroto",
+//   "Porotos Rojos",
+//   2500,
+//   "url:pic",
+//   "002",
+//   200
+// );
 
-newCollection.addProduct(
-  "Porotos",
-  "Porotos Blancos",
-  2500,
-  "url:pic",
-  "003",
-  150
-);
+// newCollection.addProduct(
+//   "Porotos",
+//   "Porotos Blancos",
+//   2500,
+//   "url:pic",
+//   "003",
+//   150
+// );
 
-newCollection.addProduct(
-  "Porotos",
-  "Porotos verdes",
-  2500,
-  "url:pic",
-  "004",
-  300
-);
+// newCollection.addProduct(
+//   "Porotos",
+//   "Porotos verdes",
+//   2500,
+//   "url:pic",
+//   "004",
+//   300
+// );
 
-newCollection.addProduct(
-  "Porotos",
-  "Porotos Naranja",
-  2500,
-  "url:pic",
-  "005",
-  0
-);
+// newCollection.addProduct(
+//   "Porotos",
+//   "Porotos Naranja",
+//   2500,
+//   "url:pic",
+//   "005",
+//   0
+// );
 
-console.log(newCollection.getProducts());
+// console.log(newCollection.getProducts());
 
-newCollection.addProduct("Porotos", "Porotos Rosa", 3000, "url:pic", "002");
+// newCollection.addProduct("Porotos", "Porotos Rosa", 3000, "url:pic", "002");
 
-//console.log(newCollection.getProducts());
-console.log(newCollection.getProductsById(5));
+// console.log(newCollection.getProducts());
+// console.log(newCollection.getProductsById(1));
+
+
+// newCollection.deleteProductoById(1)
+
+newCollection.updateProduct(4, { stock: 10})
+
+newCollection.getProducts()
